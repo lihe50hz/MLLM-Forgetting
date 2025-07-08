0e7d385e127e4f5c16d8f8c326bef219b70f1cae
lihe50hz <lihe50hz@gmail.com>
2025-07-07 23:37:12 -0700

    some update

--- launch.sh
+++ launch.sh
@@ -1,6 +1,7 @@
 export WANDB_PROJECT="VLM-Transfer"
 
 llamafactory-cli train LoRA/Qwen2.5-VL-3B.yaml
+llamafactory-cli train LoRA/Qwen2.5-VL-7B.yaml
 
 # llamafactory-cli train Eval/Qwen2.5-VL-3B.yaml
 # llamafactory-cli train Eval/Qwen2.5-VL-7B.yaml
