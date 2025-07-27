0273217da68f64b719b3dad2072344fc1e59e617
lihe50hz <lihe50hz@gmail.com>
2025-07-26 20:41:37 -0700

    test for llava

--- scripts/eval-1.sh
+++ scripts/eval-1.sh
@@ -1,18 +1,8 @@
 export WANDB_PROJECT="VLM-Transfer"
 
-llamafactory-cli train Qwen2.5-VL-3B/temp/Encoder-highlr/imagenet-20000.yaml
-llamafactory-cli train Qwen2.5-VL-3B/temp/Encoder-highlr/imagenetv2-20000.yaml
-
-llamafactory-cli train Qwen2.5-VL-3B/temp/Encoder-highlr/flower102-30000.yaml
-llamafactory-cli train Qwen2.5-VL-3B/temp/Encoder-highlr/imagenet-30000.yaml
-llamafactory-cli train Qwen2.5-VL-3B/temp/Encoder-highlr/imagenetv2-30000.yaml
-llamafactory-cli train Qwen2.5-VL-3B/temp/Encoder-highlr/stanfordcars-30000.yaml
-
-llamafactory-cli train Qwen2.5-VL-3B/temp/Encoder-highlr/flower102-40000.yaml
-
-llamafactory-cli train Qwen2.5-VL-3B/temp/Encoder-highlr/caltech101-70000.yaml
-llamafactory-cli train Qwen2.5-VL-3B/temp/Encoder-highlr/flower102-70000.yaml
-llamafactory-cli train Qwen2.5-VL-3B/temp/Encoder-highlr/imagenet-70000.yaml
-llamafactory-cli train Qwen2.5-VL-3B/temp/Encoder-highlr/imagenetv2-70000.yaml
-llamafactory-cli train Qwen2.5-VL-3B/temp/Encoder-highlr/imagewikiqa-70000.yaml
-llamafactory-cli train Qwen2.5-VL-3B/temp/Encoder-highlr/stanfordcars-70000.yaml
+llamafactory-cli train LLaVA-1.5-7B/eval/caltech101.yaml
+llamafactory-cli train LLaVA-1.5-7B/eval/flowers102.yaml
+llamafactory-cli train LLaVA-1.5-7B/eval/imagenet.yaml
+llamafactory-cli train LLaVA-1.5-7B/eval/imagenetv2.yaml
+llamafactory-cli train LLaVA-1.5-7B/eval/imagewikiqa.yaml
+llamafactory-cli train LLaVA-1.5-7B/eval/stanfordcars.yaml
